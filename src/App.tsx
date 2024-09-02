0e1c9f8570c17e3fb7bb4592c438329d1cc48ff4
emShalaby <medo.dice@gmail.com>
2024-09-02 19:49:14 +0300

    setting up tailwind and typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "../styles/index.css";
+function App() {
+  return (
+    <div id="app">
+      <h1 className="text-3xl font-bold text-black">Memory Game</h1>
+    </div>
+  );
+}
+
+export default App;
