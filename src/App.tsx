fd53466df13ea2c2abd09b36d43899fa0530d890
emShalaby <medo.dice@gmail.com>
2024-09-03 07:17:12 +0300

    add keeping scores functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Card from "./components/UI/card";
 import Score from "./components/score";
 function App() {
   const [cards, setCards] = useState([{ name: "", url: "" }]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highestScore, setHighestScore] = useState(0);
+  const [clickedCards, setClickedCards] = useState<string[]>([]);
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=49")
       .then((response) => response.json().then((data) => data.results))
@@ -13,19 +16,39 @@ function App() {
         setCards(results);
       });
   }, []);
-  const [score, setScore] = useState(0);
-  function increaseScore() {
-    setScore((prevScore) => prevScore + 1);
-  }
-  function resetScore() {
-    setScore((prevScore) => 0);
+
+  function scoreHandler(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    const cardId = e.currentTarget.id;
+    if (clickedCards.includes(cardId)) {
+      setCurrentScore(0);
+      setClickedCards([]);
+      return true;
+    }
+    setClickedCards((prevClickedCards) => {
+      const clonedCards = prevClickedCards;
+      let newCards = clonedCards;
+      newCards.push(cardId);
+      return newCards;
+    });
+    setCurrentScore((prevScore) => {
+      const newScore = prevScore + 1;
+
+      setHighestScore((prevHighestScore) =>
+        Math.max(prevHighestScore, newScore),
+      );
+
+      // Log the new score
+      console.log(newScore);
+
+      return newScore;
+    });
   }
   return (
     <div id="app" className="flex flex-col">
       <header className="flex w-[100%] max-w-[1440px] self-center">
         <h1 className="m-3 text-3xl font-bold text-black">Memory Game</h1>
         <div className="ml-auto">
-          <Score />
+          <Score currentScore={currentScore} bestScore={highestScore} />
         </div>
       </header>
       <main className="flex w-full justify-center">
@@ -45,6 +68,7 @@ function App() {
                   ".png"
                 }
                 key={item.name}
+                onClick={(e) => scoreHandler(e)}
               />
             );
           })}
--- src/components/UI/card.tsx
+++ src/components/UI/card.tsx
@@ -8,6 +8,7 @@ export default function Card({ title, imgSrc, onClick }: ICard) {
     <button
       onClick={onClick}
       className="border-2 border-solid text-center hover:bg-gray-200"
+      id={title}
     >
       <img src={imgSrc} alt={title + "-img"} width={"200px"} />
       {title}
